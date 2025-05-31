8b7e7cb2c750d06f02d01fe00a0ddb914476bdab
johnsydev <johnsylopez02@gmail.com>
2025-05-31 11:52:50 -0600

    Creación de clase Especialidad

---
+++ Especialidad.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace PRY2_Analisis_CCSS
+{
+    public class Especialidad
+    {
+        public int id_especialidad;
+        public string nombre;
+        public bool disponible;
+
+        public Especialidad(int id, string nombre)
+        {
+            this.id_especialidad = id;
+            this.nombre = nombre;
+        }
+
+        public void AbrirEspecialidad()
+        {
+            this.disponible = true;
+        }
+
+        public void CerrarEspecialidad()
+        { 
+            this.disponible = false; 
+        }
+    }
+}
